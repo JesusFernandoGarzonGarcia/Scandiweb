5c03c0eacca0f320d80bff3dd2e4a00c0ee7e5d7
JesusFernandoGarzon <jesus.garzon@uptc.edu.co>
2022-05-30 19:14:45 -0500

    config BD

--- bootstrap.php
+++ bootstrap.php
@@ -12,10 +12,10 @@ $loader = new \Twig\Loader\FilesystemLoader(APP_DIR . './templates');
 $twig = new \Twig\Environment($loader);
 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
+$servername = "bktkvvwrxwgtwmjmtwzh-mysql.services.clever-cloud.com";
+$username = "ud4m0lll7hdaaf2x";
+$password = "lR0I59MRC1AEiGDDG7y9";
+$dbname = "bktkvvwrxwgtwmjmtwzh";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
